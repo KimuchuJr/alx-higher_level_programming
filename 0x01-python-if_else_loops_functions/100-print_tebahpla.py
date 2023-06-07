c7517b995f1fb4de02bc34892dd7bcf119c79714
KimuchuJr <kimuchukim@gmail.com>
2023-06-06 19:55:40 -0700

    A program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for b in range(122, 96, -1):
-    ch = chr(b)
-        if (b % 2 == 1):
-            ch = chr(b - (ord('a') - ord('A')))
+for i in range(122, 96, -1):
+    ch = chr(i)
+    if (i % 2 == 1):
+        ch = chr(i - (ord('a') - ord('A')))
         print("{}".format(ch), end="")
