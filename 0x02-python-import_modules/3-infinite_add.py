d6bb8c1b9f404b8a37404301351fb51e0c72edb7
KimuchuJr <kimuchukim@gmail.com>
2023-06-08 13:00:51 -0700

    A program that prints the result of arguments addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main():
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
+
+
+if __name__ == "__main__":
+    main()
