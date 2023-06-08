f2a000fffd6b04e73e215fab5ddd910980de1135
KimuchuJr <kimuchukim@gmail.com>
2023-06-08 13:37:49 -0700

    A program that prints #pythoniscool

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-
-def main():
-    __builtins__.__dict__['p,r,i,n,t'.replace(',', '')]('#pythoniscool')
-
-
-if __name__ == "__main__":
-    main()
+__builtins__.__dict__['p,r,i,n,t'.replace(',', '')]('#pythoniscool')
