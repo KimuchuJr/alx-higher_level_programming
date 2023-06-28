6ecb10e406dafb3dba7e6fd4f94ce7330e4fc1b9
KimuchuJr <kimuchukim@gmail.com>
2023-06-28 09:24:19 -0700

    An empty class that defines itself

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    The topic of today is Object Oriented Programming
+    The subtopics are Classes and objects in OOP
+"""
+
+
+class Square:
+    """ An empty Square class """
+    pass
