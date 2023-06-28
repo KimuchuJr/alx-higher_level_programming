24da3b851e5111bc00fa583d3073322eb68fcdea
KimuchuJr <kimuchukim@gmail.com>
2023-06-28 09:31:40 -0700

    A class square based on task 1

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    Classes and Objects which is a continuation of task 1
+    and adds validation for the private instance attribute
+"""
+
+
+class Square:
+    """ A Square class """
+    def __init__(self, size=0):
+        """ initialize square with a specific size
+        Args:
+            __size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
