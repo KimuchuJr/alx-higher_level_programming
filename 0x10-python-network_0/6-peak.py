cc00247aac061df0566b678773ca7ef5b998d445
KimuchuJr <kimuchukim@gmail.com>
2023-10-03 07:55:55 -0700

    Technical Interview Question

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-
-"""
-    Interview preparation exercise
-    -- Algorithm must have lowest complexity
-"""
+"""Finds a peak in a list of unsorted integers"""
 
 
-def find_peak(list_int):
-    """ find the peak in a list of integers """
-    if list_int == []:
+def find_peak(list_of_integers):
+    """A function that finds a peak in a list of unsorted integers"""
+
+    array = list_of_integers
+
+    if not array:
         return None
-    elif len(list_int) == 1:
-        return list_int[0]
-    elif len(list_int) == 2:
-        return list_int[0] if list_int[0] > list_int[1] else list_int[1]
-    else:
-        for i in range(1, (len(list_int) + 1) // 2):
-            back = len(list_int) - i - 1
-            if list_int[i] >= list_int[i - 1] and \
-               list_int[i] >= list_int[i + 1]:
-                return list_int[i]
-            elif list_int[back] >= list_int[back - 1] and list_int[back] >= \
-                    list_int[back + 1]:
-                return list_int[back]
-    return None
+
+    if len(array) == 1:
+        return array[0]
+
+    if len(array) == 2:
+        return max(array[0], array[1])
+
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = left + ((right - left) // 2)
+
+        # check if left neighbor is greater
+        if mid > 0 and array[mid] < array[mid - 1]:
+            right = mid - 1
+        # check if right neighbor is greater
+        elif mid < len(array) - 1 and array[mid] < array[mid + 1]:
+            left = mid + 1
+        else:
+            return array[mid]
